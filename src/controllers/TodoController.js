99a4782d66c98edc24fc6e56043dab76ef8dd81d
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-05-04 23:07:58 +0600

    TodoList

--- src/controllers/TodoController.js
+++ src/controllers/TodoController.js
@@ -30,4 +30,94 @@ exports.SelectTodo=(req,res)=>{
 
     })
 }
+exports.SelectTodoByStatus=(req,res)=>{
+
+    let status = req.body['status'];
+    TodoModel.find({status:status}).then((result)=>{
+
+        res.status(200).json({Status:"Todo List found!",data:result});
+
+    }).catch((error)=>{
+
+        res.status(404).json({Status:"Todo List not found!",data:error});
+
+    })
+}
+exports.SelectTodoByDate=(req,res)=>{
+
+    let username = req.headers['username'];
+    let from = req.body['from'];
+    let to = req.body['to'];
+    TodoModel.find({username:username},{created_at:{$gte:Date(from),$lte:Date(to)}}).then((result)=>{
+
+        res.status(200).json({Status:"Todo List found!",data:result});
+
+    }).catch((error)=>{
+
+        res.status(404).json({Status:"Todo List not found!",data:error});
+
+    })
+}
+
+exports.UpdateTodo=(req,res)=>{
+
+    let subject = req.body['subject'];
+    let description = req.body['description'];
+    let updated_at = Date.now();
+    let id = req.body['_id'];
+
+    let PostBody =
+    {
+        subject:subject,
+        description:description,
+        updated_at:updated_at
+
+    }
+
+    TodoModel.updateOne({_id:id},{$set:PostBody},{upsert:true}).then((result)=>{
+
+        res.status(200).json({Status:"Updated Successfully!",data:result})
+
+    }).catch((error)=>{
+
+        res.status(400).json({Status:"Update failed!",data:error})
+    })
+}
+exports.UpdateTodoStatus=(req,res)=>{
+
+    let status = req.body['status'];
+    let updated_at = Date.now();
+    let id = req.body['_id'];
+
+    let PostBody =
+    {
+        status:status,
+        updated_at:updated_at
+
+    }
+
+    TodoModel.updateOne({_id:id},{$set:PostBody},{upsert:true}).then((result)=>{
+
+        res.status(200).json({Status:"Status Updated Successfully!",data:result})
+
+    }).catch((error)=>{
+
+        res.status(400).json({Status:"Update failed!",data:error})
+    })
+}
+
+exports.DeleteTodo=(req,res)=>{
+
+
+    let id = req.body['_id'];
+
+    TodoModel.deleteOne({_id:id}).then((result)=>{
+
+        res.status(200).json({Status:"Todo deleted Successfully!",data:result})
+
+    }).catch((error)=>{
+
+        res.status(400).json({Status:"delete failed!",data:error})
+    })
+}
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -9,7 +9,12 @@ router.post("/UserLogin",UserController.UserLogin);
 router.get("/UserProfile",AuthMiddleware,UserController.UserProfile);
 router.post("/UpdateProfile",AuthMiddleware,UserController.UpdateProfile);
 router.post("/CreateTodo",AuthMiddleware,TodoController.CreateTodo);
-router.get("/SelectTodo",TodoController.SelectTodo);
-
+router.get("/SelectTodo",AuthMiddleware,TodoController.SelectTodo);
+// router.post("/UpdateTodo",TodoController.UpdateTodo);
+router.post("/UpdateTodo",AuthMiddleware,TodoController.UpdateTodo);
+router.post("/UpdateTodoStatus",AuthMiddleware,TodoController.UpdateTodoStatus);
+router.post("/DeleteTodo",AuthMiddleware,TodoController.DeleteTodo);
+router.post("/SelectTodoByStatus",AuthMiddleware,TodoController.SelectTodoByStatus);
+router.post("/SelectTodoByDate",TodoController.SelectTodoByDate);
 
 module.exports = router;
